34108c4ef50b331a0e829211a61d7050a78212e3
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-04-17 21:28:26 -0700

    changed the basic neuron to using numpy dot product with matrices and vectors

--- neuron.py
+++ neuron.py
@@ -1,15 +1,12 @@
+import numpy as np
+
 inputs = [1, 3, 4, 6]
 
-weights = [1, 2, 34, 54]
+weights = [[1, 2, 34, 54], [4, 3, 23, 14], [65, 56, 3, 56]]
 
-bias = 0.45
-# 4 inputs therefore 4 weights and 1 bias since one neuron is takinfg the input and producing the output
-output = (
-    inputs[0] * weights[0]
-    + inputs[1] * weights[1]
-    + inputs[2] * weights[2]
-    + inputs[3] * weights[3]
-    + bias
-)
+bias = [2, 3, 0.6]
+
+output = np.dot(weights, inputs) + bias
+
 
 print(output)
